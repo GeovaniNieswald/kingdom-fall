cfd47b9ee8f623fc9433a6d96ef8c129220f86ab
Daniel Frey <daniel._.frey@hotmail.com>
2020-06-24 21:35:05 -0300

    cenario e personagem numero 1
    parte inicial do cenario
    movimentos do personagem
    e camera seguindo personagem

---
+++ Kigndom Fall/Assets/1 Kigndon/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour{
+
+    public Transform target;
+
+    private Vector3 velocity = Vector3.zero;
+
+    public float smoothTime = 0.3f;
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        Vector3 vector = Vector3.SmoothDamp(transform.position, target.position, ref velocity, smoothTime);
+        transform.position = new Vector3(vector.x, Mathf.Clamp(vector.y, 0, vector.y), transform.position.z);
+    }
+}
---
+++ Kigndom Fall/Assets/1 Kigndon/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+
+    private Rigidbody2D body;
+    private Animator animator;
+    private SpriteRenderer sprite;
+    public float runSpeed = 10;
+
+    public CircleCollider2D foot;
+
+    public LayerMask ground;
+
+    public float jumpForce = 200;
+
+    // Start is called before the first frame update
+    void Start()    {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()    {
+        GetForwardInput();
+        GetJumpInput();
+    }
+
+    private void GetJumpInput(){
+
+        if (Input.GetButtonDown("Jump") && foot.IsTouchingLayers(ground)) {
+            body.AddForce(Vector2.up * jumpForce);
+        }
+        animator.SetBool("IsOnGrond", foot.IsTouchingLayers(ground));
+    }
+
+    private void GetForwardInput(){
+
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * runSpeed, body.velocity.y);
+        animator.SetFloat("xSpeed", Mathf.Abs(body.velocity.x));
+        if(!sprite.flipX && body.velocity.x < 0) {
+            sprite.flipX = true;
+        }else if (sprite.flipX && body.velocity.x > 0) {
+            sprite.flipX = false;
+        }
+    }
+
+    
+}
